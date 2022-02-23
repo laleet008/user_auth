71b4cc3c767f46be8be9ad5e68efb45b34dfde1a
Lalit Puri <laleetpuree5768@gmail.com>
2022-02-23 20:58:14 +0530

    final commit

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { useUserAuth } from "../context/UserAuthContext";
+import Navbar from "./Navbar";
 
 const Dashboard = () => {
   const { user, logOut } = useUserAuth();
@@ -14,29 +15,37 @@ const Dashboard = () => {
 
   return (
     <div>
-      <div className="mt-80 flex flex-col items-center justify-center">
-        <h1 className="text-4xl">Hello, </h1>
-        <p>{user && user.email}</p>
-      </div>
-      <div className="flex justify-center mt-32">
-        <button
-          class="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded inline-flex items-center"
-          onClick={handleLogOut}
-        >
-          <svg
-            class="ml-2 mr-5 w-5 h-5"
-            fill="currentColor"
-            viewBox="0 0 20 20"
-            xmlns="http://www.w3.org/2000/svg"
+      <Navbar />
+      <div className="bg-gray-200 h-screen ">
+        <div className=" flex flex-col items-center justify-center ">
+          <h1 className="text-4xl mt-16">Hello, 🙋‍♂️ </h1>
+          <span className="text-3xl mt-500">
+            You signed in as:{" "}
+            <small className="text-xl bg-red-500 text-white">
+              {user && user.email}
+            </small>
+          </span>
+        </div>
+        <div className="flex justify-center mt-32">
+          <button
+            class="bg-blue-500 hover:bg-transparent text-gray-800 font-bold py-2 px-4 rounded inline-flex items-center"
+            onClick={handleLogOut}
           >
-            <path
-              fill-rule="evenodd"
-              d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
-              clip-rule="evenodd"
-            ></path>
-          </svg>
-          <span>Log Out</span>
-        </button>
+            <svg
+              class="ml-2 mr-5 w-5 h-5"
+              fill="currentColor"
+              viewBox="0 0 20 20"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                fill-rule="evenodd"
+                d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
+                clip-rule="evenodd"
+              ></path>
+            </svg>
+            <span>Log Out</span>
+          </button>
+        </div>
       </div>
     </div>
   );
--- src/components/Login.js
+++ src/components/Login.js
@@ -34,11 +34,11 @@ const Login = () => {
   return (
     <>
       <main>
-        <section className="absolute w-full h-full">
+        <section className="absolute w-full h-full bg-purple-300">
           <div className="container mx-auto px-4 h-full">
             <div className="flex content-center items-center justify-center h-full">
               <div className="w-full lg:w-4/12 px-4">
-                <div className="relative flex flex-col min-w-0 break-words w-full mb-6 shadow-lg rounded-lg bg-gray-300 border-0">
+                <div className="relative flex flex-col min-w-0 break-words w-full mb-6 shadow-xl rounded-lg bg-neutral-200  border-0">
                   <div className="flex-auto px-4 lg:px-10 py-10 pt-0">
                     {error && (
                       <div role="alert">
@@ -121,7 +121,7 @@ const Login = () => {
                     <a
                       href="#pablo"
                       onClick={(e) => e.preventDefault()}
-                      className="text-gray-300"
+                      className="text-gray-500"
                     >
                       <small>Forgot password?</small>
                     </a>
